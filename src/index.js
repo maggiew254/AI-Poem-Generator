2c278ecf8c7fb02f3504152f05ef620388fae6c8
Maggie <114643097+maggiew254@users.noreply.github.com>
2025-11-12 17:28:20 +0300

    added js for the poem to display

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function displayPoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "This is the poem you are looking for",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+  //let prompt = "";
+  //let apiUrl =
+  //  "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}";
+  //let apiKey = "24a6f34aet30b286677e3ofdb3c4aea0";
+}
+let poemForm = document.querySelector("#poem-form");
+poemForm.addEventListener("submit", displayPoem);
