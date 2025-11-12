cfeea0de95ed6c31ac5b3b18c1c8776a5041ff01
Maggie <114643097+maggiew254@users.noreply.github.com>
2025-11-12 18:40:25 +0300

    Added axios and integrated AI api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-function displayPoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings:
-      "This is the poem you are looking for. Roses are red, violets are blue. Sugar is sweet and so are you",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function getPoem(event) {
+  event.preventDefault();
+  let message = document.querySelector("#poem");
+  message.innerHTML = "Generating poem...";
+  let userInput = document.querySelector(".input-suggestion");
+  let apiKey = "24a6f34aet30b286677e3ofdb3c4aea0";
+  let prompt = `Please use ${userInput.value} as a subject to generate a short poem.`;
+  let context =
+    "You love poetry. Please include only a short poem with no more than 6 sentences. Separate each sentence with a <br/>. At the end of the poem, please include an extra <br/> and then sign it using SheCodes AI in a <strong> element ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  //let prompt = "";
-  //let apiUrl =
-  //  "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}";
-  //let apiKey = "24a6f34aet30b286677e3ofdb3c4aea0";
+  axios.get(apiUrl).then(displayPoem);
 }
 let poemForm = document.querySelector("#poem-form");
-poemForm.addEventListener("submit", displayPoem);
+poemForm.addEventListener("submit", getPoem);
