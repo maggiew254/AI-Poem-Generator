498205f087c92446961cb9ea5b7451b5a13ea5ae
Maggie <114643097+maggiew254@users.noreply.github.com>
2025-11-17 18:38:45 +0300

    updated AI prompt in index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function getPoem(event) {
   let apiKey = "24a6f34aet30b286677e3ofdb3c4aea0";
   let prompt = `Please use ${userInput.value} as a subject to generate a short poem.`;
   let context =
-    "You love poetry. Please include only a short poem with no more than 6 sentences. Separate each sentence with a <br/>. At the end of the poem, please include an extra <br/> and then sign it using SheCodes AI in a <strong> element ";
+    "Imagine that are an eight year old girl and you have an assigment to write a poem that is eight sentences long. Please include only a short literal poem with no more than 8 sentences. Separate each sentence with a <br/>. At the end of the poem, please include an extra <br/> and then sign it using SheCodes AI in a <strong> element ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
